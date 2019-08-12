2bec78c8aeb9375d10cc6678e60e89957c682e22
vishwanathsrikanth <vism@microsoft.com>
2019-08-13 00:05:14 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootsample/SpringbootsampleApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class SpringbootsampleApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringbootsampleApplication.class, args);
+	}
+
+}
+
