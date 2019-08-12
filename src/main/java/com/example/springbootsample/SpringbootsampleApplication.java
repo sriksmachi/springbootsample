8a56f4b74accbcc668943363229fc99d95b9c4b7
vishwanathsrikanth <vism@microsoft.com>
2019-08-13 01:06:13 +0530

    tested post and get apis

--- src/main/java/com/example/springbootsample/SpringbootsampleApplication.java
+++ src/main/java/com/example/springbootsample/SpringbootsampleApplication.java
@@ -2,8 +2,12 @@ package com.example.springbootsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 
+@EnableJpaRepositories("com.example.springbootsample.repositories")
+@EntityScan("com.example.springbootsample.model")
 @SpringBootApplication
 public class SpringbootsampleApplication {
 
